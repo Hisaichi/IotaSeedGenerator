11c61d36d021163f8a9ba1e9871ac01af95e3496
Hisaichi <32514763+Hisaichi@users.noreply.github.com>
2017-10-05 00:15:53 +0200

    Initial commit

---
+++ IotaSeedGenerator/MainWindow-Graphic.xaml.cs
@@ -0,0 +1,124 @@
+﻿// This file contain the GUI specific code of the MainWindow class
+
+using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace IotaSeedGenerator
+{
+    public partial class MainWindow : Window
+    {
+        List<TextBox> textBoxesSeed = new List<TextBox>(); // list of the text Boxes containing seeds
+        List<Button> seedButtons = new List<Button>(); // list of the buttons assoaciated with each text Box containing seeds
+
+        // Reset the seed textBoxes to normal (color, text, ...)
+		private void resetSeedBoxes()
+        {
+            int i = 0;
+            foreach (TextBox textBoxSeed in textBoxesSeed)
+            {
+                if (0 == ++i % 2) textBoxSeed.Background = Brushes.LightGray;
+                else textBoxSeed.Background = Brushes.White;
+                textBoxSeed.Foreground = Brushes.Black;
+            }
+        }
+
+        // Reset the seed buttons to normal (color, text, ...)
+		private void resetSeedButtons()
+        {
+            int i = 0;
+            foreach (Button button in seedButtons) button.Content = "Pick";
+        }
+
+        // Fill the textBoxes with seeds
+		private void fillTextBoxes()
+        {
+            const int nbOccurences = 1111;
+            foreach (TextBox textBoxSeed in textBoxesSeed) textBoxSeed.Text = getOneSeed(nbOccurences + new Random().Next(0, 177));
+        }
+
+        // What happens when one click on a button near a seed
+		private void seedButtonAction(object sender, TextBox tb)
+        {
+            Clipboard.SetText(tb.Text); // Copy the seed to the clipboard
+            resetSeedBoxes(); // Reset the seed textBoxes to normal (color, text, ...)
+            resetSeedButtons(); // Reset the seed buttons to normal (color, text, ...)
+            tb.Background = Brushes.DarkMagenta; // Color the clicked TextBox background in black
+            tb.Foreground = Brushes.White; // Color the clicked TextBox text in white
+            Button b = (Button)sender; // Convert the sender oject into button, cause yes, it's a button
+            b.Content = "Piked !"; // Assign a cool text to the button clicked
+        }
+
+        private void generate_button_Click(object sender, RoutedEventArgs e)
+        {
+            fillTextBoxes(); // Fill the textBoxes with seeds
+            resetSeedBoxes(); // Reset the seed textBoxes to normal (color, text, ...)
+            resetSeedButtons(); // Reset the seed buttons to normal (color, text, ...)
+        }
+
+        // That's not pretty, that's why it's hidden here at the bottom.
+		// It chooses colors, mainly, and create both the textBoxesSeed list and the seedButtons list.
+		// Dealing with list makes it easier to code and read.
+		private void initGraphic()
+        {
+            // textBoxesSeed list
+			textBoxesSeed.Add(textBoxSeed1);
+            textBoxesSeed.Add(textBoxSeed2);
+            textBoxesSeed.Add(textBoxSeed3);
+            textBoxesSeed.Add(textBoxSeed4);
+            textBoxesSeed.Add(textBoxSeed5);
+            textBoxesSeed.Add(textBoxSeed6);
+            textBoxesSeed.Add(textBoxSeed7);
+            textBoxesSeed.Add(textBoxSeed8);
+            textBoxesSeed.Add(textBoxSeed9);
+            textBoxesSeed.Add(textBoxSeed10);
+            textBoxesSeed.Add(textBoxSeed11);
+            textBoxesSeed.Add(textBoxSeed12);
+            textBoxesSeed.Add(textBoxSeed13);
+            textBoxesSeed.Add(textBoxSeed14);
+            textBoxesSeed.Add(textBoxSeed15);
+            textBoxesSeed.Add(textBoxSeed16);
+
+            // seedButtons list
+            seedButtons.Add(seedButton1);
+            seedButtons.Add(seedButton2);
+            seedButtons.Add(seedButton3);
+            seedButtons.Add(seedButton4);
+            seedButtons.Add(seedButton5);
+            seedButtons.Add(seedButton6);
+            seedButtons.Add(seedButton7);
+            seedButtons.Add(seedButton8);
+            seedButtons.Add(seedButton9);
+            seedButtons.Add(seedButton10);
+            seedButtons.Add(seedButton11);
+            seedButtons.Add(seedButton12);
+            seedButtons.Add(seedButton13);
+            seedButtons.Add(seedButton14);
+            seedButtons.Add(seedButton15);
+            seedButtons.Add(seedButton16);
+
+            resetSeedBoxes(); // Correct 
+            resetSeedButtons();
+        }
+        
+        // Action associated with the click for each button
+		private void Button_Click_1(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed1); }
+        private void Button_Click_2(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed2); }
+        private void Button_Click_3(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed3); }
+        private void Button_Click_4(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed4); }
+        private void Button_Click_5(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed5); }
+        private void Button_Click_6(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed6); }
+        private void Button_Click_7(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed7); }
+        private void Button_Click_8(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed8); }
+        private void Button_Click_9(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed9); }
+        private void Button_Click_10(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed10); }
+        private void Button_Click_11(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed11); }
+        private void Button_Click_12(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed12); }
+        private void Button_Click_13(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed13); }
+        private void Button_Click_14(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed14); }
+        private void Button_Click_15(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed15); }
+        private void Button_Click_16(object sender, RoutedEventArgs e) { seedButtonAction(sender, textBoxSeed16); }
+    }
+}
---
+++ IotaSeedGenerator/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿// This file contain the functionnal code of the MainWindow class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IotaSeedGenerator
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Random numbers generator to create entropy
+		Random rnd1; // Random numbers generator 1
+        Random rnd2; // Random numbers generator 2
+        Random rnd3; // Random numbers generator 3
+
+        const int SEED_LEN = 81; // Recommended length for a iota key
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Visibility = Visibility.Hidden; // Hide the window until it's fully arty
+            initGraphic(); // Doing the making up
+            rnd1 = new Random();
+            Thread.Sleep(100); // If initialized too fast one after another, it seems the generator are somewhat producing similar numbers
+            rnd2 = new Random();
+            Thread.Sleep(150); // ""
+            rnd3 = new Random();
+            Thread.Sleep(100); // ""
+            fillTextBoxes(); // Generating seeds, that's why we're here for anyway
+            Visibility = Visibility.Visible; // Et voilà !
+        }
+
+        // This function generates one seed in a single iteration and returns it
+        string getOneSeed()
+        {
+            // We create some entropy
+            int timeOffset = (int)DateTime.Now.Ticks;
+
+            // 4 times the same caracters in different orders, in order to create some entropy
+            List<string> authorizedCharList = new List<string>(new string[] { "YHNUJIKOLPMAQWZSXEDCRFVTGB9", "9ABCDEFGHIJKLMNOPQRSTUVWXYZ", "QWERTYUIOPASDF9GHJKLMZXCVBN", "ZBSOLDE9HUXYANJIWQGKMPFVTRC" });
+
+            string seed = "";
+            Random rnd = new Random();
+
+            // We build the seed using each list right above, one at a time.
+			// Both the time offset (DateTime.Now.Ticks) and the random generator are used to chose the letters
+            for (int i = 0; i < SEED_LEN; i++)
+            {
+                int index = Math.Abs(timeOffset + rnd.Next(0))%authorizedCharList.Count;
+                seed += authorizedCharList[index][rnd1.Next(0, authorizedCharList[index].Length)];
+            }
+
+            return seed;
+        }
+
+        // This function generates one seed using "v" iterations and returns it
+        private string getOneSeed(int v)
+        {
+            string seed = getOneSeed();
+
+            // Mixing seeds v times
+			for (int i = 0; i < v; i++) seed = mixSeed(seed, getOneSeed());
+
+            return seed;
+        }
+
+        // This function mixes the seed1 with the seed2 and returns the result.
+        // For each caracter, it randomly select the corresponding caractère of seed1 or seed2.
+        private string mixSeed(string seed1, string seed2)
+        {
+            string seed = "";
+
+            Random rnd = new Random();
+
+            // We go through all the strings seed1 and seed2, randomly picking up a caracter from one or
+            // the other in order to build the new seed.
+            for (int i = 0; i < SEED_LEN ; i++)
+            {
+                if (0 == rnd.Next() % 2) seed += seed1[i];
+                else seed += seed2[i];
+            }
+
+            return seed;
+        }
+    }
+}
